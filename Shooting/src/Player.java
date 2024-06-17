03f04de3d03c1b6164179eebb48dd7f488110209
kaori23410157 <0cc42x022p.2v6j@au.com>
2024-06-17 11:11:16 +0900

    10-6画面外に出た弾を消す

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -19,6 +19,8 @@ public class Player extends Character implements KeyListener{
 		if(e.getKeyCode()==KeyEvent.VK_SPACE) {
 			GameWorld.playerBullets.add(
 					new PlayerBullet(x,y,0,-10));
+			System.out.println("弾の数="+
+					GameWorld.playerBullets.size());
 		}
 	}
 	public void keylessed(KeyEvent e) {
