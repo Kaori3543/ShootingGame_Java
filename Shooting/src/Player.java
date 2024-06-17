874f867de12db1d1e138be574997513791360c1f
kaori23410157 <0cc42x022p.2v6j@au.com>
2024-06-17 10:33:38 +0900

    10-4プレイヤーをキーボードで動かす

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -1,7 +1,40 @@
-
-public class Player extends Character{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class Player extends Character implements KeyListener{
 	public Player(double x,double y,double vx,double vy) {
 		super(x,y,vx,vy);
 	}
+	public void move() {
+		super.move();
+		if(x<0) x=0;
+		if(x>370) x=370;
+	}
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=-5;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=5;
+		}
+	}
+	public void keylessed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=0;
+		}
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
 
 }
