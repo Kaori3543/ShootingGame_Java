03751a90ad864a31e89938aad9ace99484e32afa
kaori23410157 <0cc42x022p.2v6j@au.com>
2024-06-24 11:38:09 +0900

    12-2プレイヤー弾と敵の衝突判定

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -13,18 +13,56 @@ public class GameFrame extends MyFrame{
 		GameWorld.player.move();
 		
 		movePlayerBullets();
+		mioveEnemies();
+		checkPlayerAndEnemies();
+		checkPlayerBulletsAndEnemies();
+		sleep(0.03);
 		for(int i=0; i<GameWorld.enemies.size(); i++) {
 			Enemy e=GameWorld.enemies.get(i);
-			if(Math.abs(e.x-GameWorld.player.x)<=30 &&
-				Math.abs(e.y-GameWorld.player.y)<=30) {
-				System.out.println("やらてた!");
+			if(e.x==GameWorld.player.x &&
+					e.y==GameWorld.player.y) {
+				System.out.println("やられた!");
 			}
-			e.draw(this);
-			e.move();
 		}
 		sleep(0.03);
 	}
 }
+	private void checkPlayerAndEnemies() {
+		for(int i=0; i<GameWorld.enemies.size(); i++) {
+			Enemy e=GameWorld.enemies.get(i);
+			if(Math.abs(e.x-GameWorld.player.x)<=30 &&
+					Math.abs(e.y-GameWorld.player.y)<=30) {
+				System.out.println("やられた!");
+				GameWorld.player.y=-1000;
+			}
+		}
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	public void checkPlayerBulletsAndEnemies() {
+		int i=0;
+		while(i<GameWorld.playerBullets.size()) {
+			PlayerBullet b=GameWorld.playerBullets.get(i);
+			int j=0;
+			int hits=0;
+			while(j<GameWorld.enemies.size()) {
+				Enemy e=GameWorld.enemies.get(j);
+				if(Math.abs(e.x-b.x)<=30 &&
+						Math.abs(e.y-b.y)<=30) {
+					System.out.println("あたり");
+					hits++;
+					GameWorld.enemies.remove(j);
+				}else {
+					j++;
+				}
+			}
+			if(hits>0) {
+				GameWorld.playerBullets.remove(i);
+			}else {
+				i++;
+			}
+		}
+	}
 public void mioveEnemies() {
 	for(int i=0; i<GameWorld.enemies.size(); i++) {
 		Enemy e=GameWorld.enemies.get(i);
